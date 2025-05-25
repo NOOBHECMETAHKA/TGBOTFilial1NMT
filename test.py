692b281587d46d116f5d503b32fb4e5d9918cdc4
NOOBHECMETAHKA <ebaltboupotha@gmail.com>
2025-05-26 01:04:05 +0300

    vertion, test

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-from Modules.TelegramNavigationMenuManager import TGNavigationMenuManager
+import unittest
+import pyodbc
+import os.path as fileManager
+from Modules.WorkingTeamInformationCollection import WorkingTeamInformation
+from Modules.DataBaseManager import DataBaseManager
 
-menu = TGNavigationMenuManager()
-for element in menu.main_menu_content[5]["menu"]:
-    print(element)
+class PreliminaryTesting(unittest.TestCase):
+    def setUp(self):
+        self.file_working_team = WorkingTeamInformation().file_path_of_working_class
+        self.file_database_manager = DataBaseManager().MAIN_PATH
+    
+    
+    def test_check_file_working_team(self):
+        '''Проверяет местоположние файла рабочей команды'''
+        self.assertTrue(fileManager.exists(self.file_working_team), "Файл рабочки не найден!")
+    
+    def test_check_file_database(self):
+        '''Проверяет местоположение файла базы данных'''
+        self.assertTrue(fileManager.exists(self.file_database_manager), "Файл базы данных не найден!")
+    
+    def test_check_necessary_database_drivers(self):
+        '''Проверяет доступность необходимого драйвера для работы с базой данных'''
+        necessary_driver = "Microsoft Access Driver (*.mdb, *.accdb)"
+        driver_pack_pyobc = pyodbc.drivers()
+        self.assertIn(necessary_driver, driver_pack_pyobc)
+    
+
+#python -m unittest test.py  
